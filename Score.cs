99ded15b87089f2d0c49b5edda2bb0473f5ce540
LakshmiNarayanan P <50983609+LakshmiNarayanan28@users.noreply.github.com>
2019-07-13 21:06:44 +0530

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	private float score=0.0f;
+	private int difficultyLevel = 1;
+	private int maxDiffcultyLevel =10;
+	private int scoreToNextLevel = 10;
+	
+	private bool isDead = false;
+	public Text scoreText;
+	public DeathMenu deathMenu;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(isDead)
+		return;
+	
+		if(score >= scoreToNextLevel)
+			LevelUp();
+		score +=Time.deltaTime * difficultyLevel;
+        scoreText.text = ((int)score).ToString ();
+  }
+  
+  void LevelUp()
+  {
+	  
+	  if(difficultyLevel==maxDiffcultyLevel)
+		  return;
+	  
+	  scoreToNextLevel*=2;
+	  difficultyLevel++;
+	  
+	  GetComponent<pl>().SetSpeed (difficultyLevel);
+	  Debug.Log(difficultyLevel); 
+	  }
+	  public void OnDeath()
+	  {
+		 isDead = true;
+		 if( PlayerPrefs.GetFloat("Highscore") < score)
+		 PlayerPrefs.SetFloat("Highscore",score);
+		
+		 deathMenu.ToggleEndMenu (score);
+	  }
+}
---
+++ pl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pl : MonoBehaviour
+{
+	private CharacterController controller;
+	private Vector3 moveVector;
+	private float speed=5.0f;
+	private float verticalVelocity= 0.0f;
+	private float gravity= 12.0f;
+	private float animationDuration = 3.0f;
+	private float startTime;
+	
+	private bool isDead = false;
+	
+    // Start is called before the first frame update
+    void Start()
+
+    {
+		controller=GetComponent<CharacterController> ();
+		startTime=Time.time;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		if(isDead)
+			return;
+		
+		if(Time.time -startTime < animationDuration)
+		{
+			controller.Move(Vector3.forward * speed * Time.deltaTime);
+			return;
+		}
+		moveVector=Vector3.zero;
+		
+		if(controller.isGrounded)
+		{
+			 verticalVelocity =-0.5f;
+		}
+		else
+		{
+			verticalVelocity -= gravity * Time.deltaTime;
+		}
+		//x -Left and Right
+		moveVector.x =Input.GetAxisRaw("Vertical") * speed;
+		//y-Up and Down
+		
+		moveVector.y=verticalVelocity;
+		
+		//z - Forward and Backward
+		moveVector.z=speed;
+		
+        controller.Move(moveVector * Time.deltaTime);
+    }
+	public void SetSpeed(float modifier)
+	{
+		speed = 5.0f + modifier;
+	}
+	
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if(hit.point.z > transform.position.z + (controller.radius/2)) //set as per our game
+		{
+			if (hit.gameObject.tag == "obstacles"){
+			Death();
+			}
+		}
+	}
+	private void Death()
+	{
+		
+		 isDead = true;
+		  GetComponent<Score> ().OnDeath();
+	}
+}
